f92aa61d58afa82f9fca6a2b10c96a4fcf27cb2b
azizHr <azizharkati20@gmail.com>
2023-12-08 10:55:35 +0100

    add content for [Fish] Model

--- src/main/java/com/dev/aftas/model/Fish.java
+++ src/main/java/com/dev/aftas/model/Fish.java
@@ -1,4 +1,17 @@
 package com.dev.aftas.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
 public class Fish {
+
+    @Id
+    private String name;
+    private Double averageWeight;
+
 }
