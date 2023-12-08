f18f290bb53ce0e5f86dcacd356721b672be88cd
azizHr <azizharkati20@gmail.com>
2023-12-08 10:53:50 +0100

    add content for [Hunting] Model

--- src/main/java/com/dev/aftas/model/Hunting.java
+++ src/main/java/com/dev/aftas/model/Hunting.java
@@ -1,4 +1,19 @@
 package com.dev.aftas.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
 public class Hunting {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private Integer numberOfFish;
+
 }
