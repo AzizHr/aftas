4166c8d232c4d28c4b48c17e5a807d9c023d7b27
azizHr <azizharkati20@gmail.com>
2023-12-08 10:11:48 +0100

    create spring boot project [api]

---
+++ src/main/java/com/dev/aftas/AftasApplication.java
@@ -0,0 +1,13 @@
+package com.dev.aftas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AftasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AftasApplication.class, args);
+    }
+
+}
