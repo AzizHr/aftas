af58f08bf0588922bdfb55b94718d3a3caded5ac
azizHr <azizharkati20@gmail.com>
2023-12-08 10:51:31 +0100

    add content for [Competition] Model

--- src/main/java/com/dev/aftas/model/Competition.java
+++ src/main/java/com/dev/aftas/model/Competition.java
@@ -1,4 +1,25 @@
 package com.dev.aftas.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data
+@NoArgsConstructor
+@Entity
 public class Competition {
+
+    @Id
+    private String code;
+    private LocalDate date;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private Integer numberOfParticipants;
+    private String location;
+    private Double amount;
+
 }
