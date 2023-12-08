7e90574fdd1e22e519d9926780d0f5d477f394ea
azizHr <azizharkati20@gmail.com>
2023-12-08 10:45:16 +0100

    add content for [Member] Model

--- src/main/java/com/dev/aftas/model/Member.java
+++ src/main/java/com/dev/aftas/model/Member.java
@@ -1,4 +1,27 @@
 package com.dev.aftas.model;
 
+import com.dev.aftas._enum.IdentityDocumentType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@Entity
 public class Member {
+
+    @Id
+    @GeneratedValue
+    private Integer num;
+    private String name;
+    private String familyName;
+    private LocalDate accessionDate;
+    private String nationality;
+    private IdentityDocumentType identityDocument;
+    private String identityNumber;
+
 }
