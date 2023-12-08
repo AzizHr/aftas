d924a9b5c3d033f049f5346277600b8b5164024a
azizHr <azizharkati20@gmail.com>
2023-12-08 15:23:07 +0100

    adding huntingList field in the fish Model

--- src/main/java/com/dev/aftas/model/Fish.java
+++ src/main/java/com/dev/aftas/model/Fish.java
@@ -1,9 +1,13 @@
 package com.dev.aftas.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -13,5 +17,7 @@ public class Fish {
     @Id
     private String name;
     private Double averageWeight;
+    @OneToMany(mappedBy = "fish", cascade = CascadeType.REMOVE)
+    private List<Hunting> huntingList;
 
 }
